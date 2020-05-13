f97e5c7b63bc5565c9bb0a9f4bbc65cf92a5d059
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-12 22:22:21 -0400

    Commi, guardar programa

---
+++ Lab6POO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab6POO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
