0c7dccb95408e9597a7c3d1d180cf6a7f6a5759b
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-05-15 01:02:06 -0400

    Parte I lista

---
+++ Lab6POO/Empresa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Lab6POO
+{
+    [Serializable]
+    public class Empresa
+    {
+
+        private string name;
+        private string rut;
+        public Empresa(string Name, string Rut)
+        {
+            this.name = Name;
+            this.rut = Rut;
+        }
+        public string Name { get => name; set => name = value; }
+        public string Rut { get => rut; set => rut = value; }
+
+        
+    }
+}
--- Lab6POO/Program.cs
+++ Lab6POO/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Lab6POO
 {
@@ -6,7 +10,93 @@ namespace Lab6POO
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string respuesta;
+            string nombreem;
+            string rutem;
+            List<Empresa> empresas = new List<Empresa> { };
+            while (true)
+            {
+                Console.WriteLine("Buenos días");
+                Console.WriteLine("¿Desea cargar una empresa desde el archivo?");
+                respuesta = Console.ReadLine();
+           
+
+                if (respuesta == "No" || respuesta == "no")
+                {
+                    Console.WriteLine("Ingrese el nombre de la empresa:");
+                    nombreem = Console.ReadLine();
+                    Console.WriteLine("Ingrese el rut de la empresa:");
+                    rutem = Console.ReadLine();
+                    Empresa empresa = new Empresa(nombreem, rutem);
+                    empresas.Add(empresa);
+                    Save(empresas);
+
+
+
+
+
+
+                }
+                if (respuesta == "Si" || respuesta == "si")
+                {
+                    try
+                    {
+                        empresas = Load();
+                        showEnterprises(empresas);
+                    }
+                    catch(FileNotFoundException)
+                    {
+                        Console.WriteLine("No pudimos encontrar a la empresa\n");
+                        Console.WriteLine("Favor ingrese los datos de la empresa que desea agregar:");
+
+                        Console.WriteLine("Nombre:");
+                        nombreem = Console.ReadLine();
+                        Console.WriteLine("Rut:");
+                        rutem = Console.ReadLine();
+                        Empresa empresafnf = new Empresa(nombreem, rutem);
+                        empresas.Add(empresafnf);
+                        Save(empresas);
+                        
+
+                    }
+                }
+                if (respuesta=="kie")
+                {
+                    break;
+                }
+
+
+
+
+
+            }
+        }
+
+        static public void showEnterprises(List<Empresa> empresas)
+        {
+            foreach (Empresa empresa in empresas)
+            {
+                Console.WriteLine("Nombre: "+empresa.Name+" Rut Empresa: "+empresa.Rut);
+            }
+        }
+
+        static private void Save(List<Empresa> empresas)
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("empresa.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, empresas);
+            stream.Close();
+        }
+
+
+
+        static List<Empresa> Load()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream("empresa.bin", FileMode.Open, FileAccess.Read, FileShare.Read);
+            List<Empresa> empresas = (List<Empresa>)formatter.Deserialize(stream);
+            stream.Close();
+            return empresas;
         }
     }
 }
